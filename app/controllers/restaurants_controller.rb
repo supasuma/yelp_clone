d345b2d3296821770f1256edc2821ab55a2af257
supasuma <supasuma@gmail.com>
2016-10-03 17:25:52 +0100

    added route for creating restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,10 +5,18 @@ class RestaurantsController < ApplicationController
   end
 
   def new
-
+    @restaurant = Restaurant.new
   end
 
   def create
-      raise params
-    end
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+
 end
