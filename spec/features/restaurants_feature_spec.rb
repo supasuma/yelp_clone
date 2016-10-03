9ffa6d539deb3de3b7d8b85b6f216f75a21b2458
matt vickers <matt.vickers@sky.com>
2016-10-03 18:00:50 +0100

    adds destroy

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -63,4 +63,17 @@ context 'editing restaurants' do
 
 end
 
+context 'deleting restaurants' do
+
+  before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
+
+  scenario 'removes a restaurant when a user clicks a delete link' do
+    visit '/restaurants'
+    click_link 'Delete KFC'
+    expect(page).not_to have_content 'KFC'
+    expect(page).to have_content 'Restaurant deleted successfully'
+  end
+
+end
+
 end
