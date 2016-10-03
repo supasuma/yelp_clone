d250f296a38aa0428adb11dd58bf0e9987bfac89
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-10-03 17:08:15 +0100

    created test for creating a new restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,14 @@
 class RestaurantsController < ApplicationController
 
   def index
-    
+    @restaurants = Restaurant.all
   end
+
+  def new
+
+  end
+
+  def create
+      raise params
+    end
 end
