438dc6b23cd198ad09290b6b6ff41ded9a5acd7b
BenRoss92 <benjaminjackross@gmail.com>
2016-10-05 10:54:03 +0100

    validates rating number and name uniqueness

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -6,4 +6,11 @@ describe Restaurant, type: :model do
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
   end
+
+  it 'is not valid if the name is not unique' do
+    Restaurant.create(name: "Moe's Tavern")
+    restaurant = Restaurant.new(name: "Moe's Tavern")
+    expect(restaurant).to have(1).error_on(:name)
+  end
+
 end
