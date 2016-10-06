dbdd9325a35d78b9b26ede8ddfdd4ed236f3544c
BenRoss92 <benjaminjackross@gmail.com>
2016-10-06 11:46:03 +0100

    restaurant ID not being recognised

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :restaurants
   has_many :reviews
   has_many :reviewed_restaurants, through: :reviews, source: :restaurant
 
